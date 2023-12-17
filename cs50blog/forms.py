063c2fba4c59ca941bb4ef383eae6797d362bf43
jasper <jasperlimsl@gmail.com>
2023-12-18 00:06:22 +0100

    Version 4 - Account page and functionalities added

--- cs50blog/forms.py
+++ cs50blog/forms.py
@@ -1,5 +1,7 @@
 # Importing the FlaskForm base class from the flask_wtf extension that must be installed using "pip install flask-wtf"
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 # the wtforms library comes with the flask-wtf extension package installed
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -28,3 +30,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=25)])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture (jpg/png)', validators=[FileAllowed(['jpg','png'])])
+    submit = SubmitField('Update')
+
+    # Using wtforms to validate and raise validationerrors.
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is already used. Please use another one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That email is already used. Please use another one.')
